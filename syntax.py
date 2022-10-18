143684834857ae984d827c694d8afd249cca1c77
Luca <lucakanyo@protonmail.com>
2022-10-18 14:05:07 -0400

    user input

--- syntax.py
+++ syntax.py
@@ -8,4 +8,12 @@
 
 #VARIABLE
 
+name = "Tim"
+
+print(type(name)) # => <class 'str'>
+print(5>10) # => False
+
+username = input("Please enter username: ") # input will be string, even if int 
+print(type(username))
+
 
