a101363379c3408d9b32688245e820bd37a2644a
Luca <lucakanyo@protonmail.com>
2022-10-18 14:16:30 -0400

    type conversion and complex num

--- syntax.py
+++ syntax.py
@@ -8,12 +8,27 @@
 
 #VARIABLE
 
-name = "Tim"
+# name = "Tim"
 
-print(type(name)) # => <class 'str'>
-print(5>10) # => False
+# print(type(name)) # => <class 'str'>
+# print(5>10) # => False
 
-username = input("Please enter username: ") # input will be string, even if int 
-print(type(username))
+# username = input("Please enter username: ") # input will be string, even if int 
+# print(type(username))
 
+# quote ="""I wandered lonely as a cloud
+# That floats on high o'er vales and hills,
+# When all at once I saw a crowd,
+# A host, of golden daffodils;
+# Beside the lake, beneath the trees,
+# Fluttering and dancing in the breeze.
+# - William Wordsworth """
+# print(quote)
 
+#int() float() complex() bool() 
+int(5.4) # => 5
+bool("Hello") # => True
+
+#COMPLEX NUMBERS 
+num = 5 + 3j
+print(num)
