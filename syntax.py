882acacad99f21b4cf4a7d770422c3468c8511ea
Luca <lucakanyo@protonmail.com>
2022-10-19 14:58:27 -0400

    and, or, not, chained

--- syntax.py
+++ syntax.py
@@ -65,3 +65,18 @@ float_or_int = 5 - 5.0 #=> returns a float when there is a float i the expressio
 print('a' > 'Z')
 #ordinal value 
 print(ord('a'), ord('Z'))
+
+7.0 == 7 #=> returns True
+z = 9
+x = 8 
+result3 = z < x + 2 # => True
+#conditional operators have a lower precedence than arithmetic operators
+
+#chain conditionals - in the order of precedence
+# not
+# and
+# or
+
+print(5 < 4 or not True and True) #=> False 
+print(5 < 4 or not True and True or True) #=> True
+print(not (True and False)) #=> True
