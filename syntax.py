8cfd201957644b63292212ee2f88b9cca04da022
Luca <lucakanyo@protonmail.com>
2022-10-21 10:59:04 -0400

    slice

--- syntax.py
+++ syntax.py
@@ -150,3 +150,12 @@ print("We've reached 20")
 while True:
     print("You don't have to stuck in an infinite loop if you don't want to!")
     break
+
+#SLICE - for collections (arrays, tuples) and strings 
+x = [3,4,5,7,9,10,17]
+#x[start:stop:step]
+sliced = x[0:4:2]
+x[2:] #starts at 2, stops at end
+x[:2] #starts at beginning, stops at 2
+x[::-1] #reverse the sting/array
+print("hello world"[::-1]) #=> dlrow olleh
