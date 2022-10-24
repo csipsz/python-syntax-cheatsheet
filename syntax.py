b63cdc40ecbe05cb4243a976b7e612e1fbf9866c
Luca <lucakanyo@protonmail.com>
2022-10-24 12:12:36 -0400

    lambda and functions as arguments

--- syntax.py
+++ syntax.py
@@ -312,7 +312,7 @@ finally:
 x = lambda x: x - 3  #not how is used, just shows functionality
 print(x(9))
 
-#map 
+#map  and filter
 
 x = [1,2,3,4,5]
 mp = map(lambda i: i - 2, x) #=> map returns a map object
@@ -320,6 +320,12 @@ print(list(mp))
 
 ft = filter(lambda i: i % 2 == 0, x) 
 print(list(ft))
+
+def func_for_map(i):
+    i = str(i) + " Yes"
+    return i
+#you can add a function as an argument, or write inline a lambda 
+print(list(map(func_for_map, x)))
 
 
 
