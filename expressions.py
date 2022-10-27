153c641c3a2b99d2aeb840f19fdfe64284418031
Luca <lucakanyo@protonmail.com>
2022-10-27 10:49:37 -0400

    order of op, expressions

---
+++ expressions.py
@@ -0,0 +1,15 @@
+result = 6 / 3 # => returns a float 
+result2 = 10 // 3 #=> floor division
+6 ** 3 # => exponent
+6 % 5 # => modulo 
+"hello" * 7
+
+#ORDER OF OPERATION 
+#BRACKETS
+#EXPONENTS
+#DIVISION
+#MULTIPLICATION
+#ADDITION
+#SUBTRACTION
+
+float_or_int = 5 - 5.0 #=> returns a float when there is a float i the expression
