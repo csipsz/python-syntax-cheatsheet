e592e717b6b2184c7a2bafd24711c0b26df9a229
Luca <lucakanyo@protonmail.com>
2022-10-28 11:49:24 -0400

    conditionals in list comprehensions

--- practice.py
+++ practice.py
@@ -1,2 +1,5 @@
 res = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
 print(res)
+
+#SWAP CASE
+#return "".join([i.upper() if i == i.lower() else i.lower() for i in s])
