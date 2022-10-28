98c21f2bc47ab849c7aca955d72218119d75890f
Luca <lucakanyo@protonmail.com>
2022-10-28 11:56:38 -0400

    split and join

--- practice.py
+++ practice.py
@@ -1,5 +1,17 @@
+x = 2
+y = 1
+z = 3
+n = 3
+
+
+
 res = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
 print(res)
 
 #SWAP CASE
 #return "".join([i.upper() if i == i.lower() else i.lower() for i in s])
+
+#splitting a string into an array, and joining it back to a string:
+helloarr = "hello world".split(" ")
+hellostring ="-".join(helloarr)
+print(hellostring)
